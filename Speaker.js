872cc1f75614b2493c33b7c5f8ae0de54819344b
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-01-08 10:16:21 -0500

    added comments, updated readme

--- Speaker.js
+++ Speaker.js
@@ -1,21 +1,33 @@
+// Load external modules. The require function is provided
+// is provided by the Node runtime. If you wanted this to
+// work in a browser, you would use something like requirejs
+// to load external modules instead.
 var MemoryBuffer = require("./MemoryBuffer");
 var _ = require("underscore");
 
+// Define a Speaker inside of a functional closure. The functional
+// closure will return its own Speaker function (object).
 var Speaker = (function (){
+  "use strict";
 
   var defaults = {
     buffer: new MemoryBuffer(),
     name: "Ann"
   };
 
+  // Speaker constructor. This takes an options argument.
   function Speaker(options) {
     this.options = _.extend({}, defaults, options);
-    this.name = this.options.name;
-    this.timesSpoken = 0;
-    this.thingsSaid = [];
-    this.numberOfThingsSaid = 0;
+    initialize(this);
+    return this;
   }
 
+  // The say method will be publicly exposed on instances of
+  // Speaker.
+  //
+  //   var speaker = new Speaker();
+  //   speaker.say("Hello");
+  //
   Speaker.prototype.say = function (toSay) {
     var message = '"' + toSay + '," said ' + this.name + '.';
     this.options.buffer.write(message);
@@ -24,6 +36,9 @@ var Speaker = (function (){
     return message;
   };
 
+  // This function will be local to the functional closure. You
+  // will be unable to access addToThingsSaid() from outside
+  // this functional closure, effectively making a private function.
   function addToThingsSaid(speaker, said) {
     if (!_.contains(speaker.thingsSaid, said)) {
       speaker.thingsSaid.push(said);
@@ -31,6 +46,17 @@ var Speaker = (function (){
     }
   }
 
+  // This is private to the functional closure, like addToThingsSaid().
+  function initialize(speaker) {
+    speaker.name = speaker.options.name;
+    speaker.timesSpoken = 0;
+    speaker.thingsSaid = [];
+    speaker.numberOfThingsSaid = 0;
+  }
+
+  // This returns the local Speaker function, exposing it
+  // outside the functional closure, along with anything that
+  // was prototyped onto the Speaker function.
   return Speaker;
 
 })();
--- readme.md
+++ readme.md
@@ -4,12 +4,18 @@ This is a demo project that I used for teaching about JavaScript objects. The pr
 
 ## Requirements
 
-Check out the [package.json]() file for more info.
+Check out the [package.json](https://github.com/jarrettmeyer/speaker.js/blob/master/package.json) file for more info.
 
-- [NodeJS]
+- [NodeJS](http://nodejs.org)
 - npm install jasmine-node
-- npm install
+- npm install underscore
 
 ## Running Tests
 
+From the command line...
+
     > jasmine-node ./specs
+
+Or
+
+    > npm run-scripts specs
